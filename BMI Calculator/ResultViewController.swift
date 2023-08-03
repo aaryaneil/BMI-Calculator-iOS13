2fc4a4c6b08e6c3c00b0c718f06ac5a82da5a2e4
Aaryaneil Nimbalkar <aaryaneil.nimbalkar@gmail.com>
2023-08-02 19:31:49 -0700

    First commit

---
+++ BMI Calculator/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Aaryaneil Nimbalkar on 7/21/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
